dc1fc4e24e95ab97cb3bcced97eef1e5e89a061c
Gayatri <gayatrisahani0316@gmail.com>
2026-07-10 00:07:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (78.18%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = static_cast<int>(s.length())-1;
+        while(left < right){
+            while((left < right) && !isalnum(static_cast<unsigned char>(s[left]))){
+                left++;
+            }
+            while((left < right) && !isalnum(static_cast<unsigned char>(s[right]))){
+                right--;
+            }
+            if(tolower(static_cast<unsigned char>(s[left])) != tolower(static_cast<unsigned char>(s[right]))) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
